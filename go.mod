70a544226b9351fe6a83db42cbc32d9e288776ea
Nel Dev <nelgonzalezferreras@gmail.com>
2023-04-27 13:38:27 +0200

    update config

--- go.mod
+++ go.mod
@@ -3,12 +3,12 @@ module keyApp
 go 1.18
 
 require (
-	gorm.io/driver/mysql v1.5.0
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/jinzhu/gorm v1.9.16
 	gorm.io/gorm v1.25.0
 )
 
 require (
-	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 )
--- internal/db/migrate.go
+++ internal/db/migrate.go
@@ -1,22 +1,33 @@
 package db
 
 import (
-    "gorm.io/driver/mysql"
+    "fmt"
+    "log"
     "gorm.io/gorm"
     "key-app-go/internal/model"
+    "github.com/go-sql-driver/mysql"
+     "github.com/jinzhu/gorm"
 )
 
 func AutoMigrate() error {
-    dsn := "keyuser:keypassword@tcp(127.0.0.1:3305)/mysql-oe2L?charset=utf8mb4&parseTime=True&loc=Local"
-    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-    if err != nil {
-        return err
-    }
 
-    err = db.AutoMigrate(&model.Key{}, &model.User{})
-    if err != nil {
-        return err
-    }
+    dbHost := "172.17.0.1"
+    dbPort := "3305"
+    dbName := "keyd"
+    dbUser := "keyuser"
+    dbPassword := "keypassword"
 
-    return nil
+    dbURI := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+    db, err := gorm.Open("mysql", dbURI)
+    if err != nil {
+        log.Fatalf("failed to connect database: %v", err)
+    }
+    defer db.Close()
+
+    db.AutoMigrate(&model.Key{})
+    db.AutoMigrate(&model.User{})
+
+    log.Println("Migration has been successfully completed!")
+
 }
