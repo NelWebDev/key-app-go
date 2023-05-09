113712bc8151d76488a7d4b2c88264f571736e73
nel.gonzalez <nel.gonzalezferreras@plexus.es>
2023-05-09 19:33:31 +0200

    update cam

--- internal/db/migrate.go
+++ internal/db/migrate.go
@@ -1,26 +1,23 @@
 package db
 
 import (
+	"fmt"
+	"key-app-go/internal/model"
 
-    "fmt"
-    "github.com/jinzhu/gorm"
-    "key-app-go/internal/model"
-        _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 )
 
 func AutoMigrate() error {
-    db, err := gorm.Open("mysql", "keyuser:keypassword@tcp(172.23.0.2:3305)/db?charset=utf8mb4&parseTime=True&loc=Local")
-    if err != nil {
-        return fmt.Errorf("error al conectar a la BD: %v", err)
-    }
+	db, err := gorm.Open("mysql", "keyuser:keypassword@tcp(172.23.0.2:3305)/db?charset=utf8mb4&parseTime=True&loc=Local")
+	if err != nil {
+		return fmt.Errorf("error al conectar a la BD: %v", err)
+	}
 
-    if err := db.AutoMigrate(&model.Key{}, &model.User{}).Error; err != nil {
-        return fmt.Errorf("error al migrar los modelos: %v", err)
-    }
+	if err := db.AutoMigrate(&model.Key{}, &model.User{}).Error; err != nil {
+		return fmt.Errorf("error al migrar los modelos: %v", err)
+	}
 
-
-    fmt.Println("Migraciones realizadass con éxito")
-    return nil
+	fmt.Println("Migraciones realizadas con éxito")
+	return nil
 }
-
-
